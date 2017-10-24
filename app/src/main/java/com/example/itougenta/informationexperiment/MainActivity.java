2b66c09264c594b68fe02f29727a20daa86536ff
Itou Genta <b1545276@planet.kanazawa-it.ac.jp>
2017-10-24 16:02:50 +0900

    first commit

---
+++ app/src/main/java/com/example/itougenta/informationexperiment/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.itougenta.informationexperiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
